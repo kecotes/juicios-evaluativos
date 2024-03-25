5a62107edaa1138af6765c52145df87d6158a78e
kecotes <kecotes@uniguajira.edu.co>
2024-03-24 19:10:51 -0500

    Hacemos que el initialState contenta el estado actual y logre persistir los datos

--- src/store/users/slice.ts
+++ src/store/users/slice.ts
@@ -1,18 +1,6 @@
 import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
 
-export type UserId = string;
-
-export interface User {
-    name: string;
-    email: string;
-    github: string;
-}
-
-export interface UserWithId extends User {
-    id: UserId;
-}
-
-const initialState: UserWithId[] = [
+const DEFAULT_STATE = [
     {
     id: "1",
     name: 'John Doe',
@@ -37,7 +25,29 @@ const initialState: UserWithId[] = [
     email: 'pelma@gmail.com',
     github: 'migithub'
   },
-]
+];
+
+export type UserId = string;
+
+export interface User {
+    name: string;
+    email: string;
+    github: string;
+}
+
+export interface UserWithId extends User {
+    id: UserId;
+}
+
+//Tenemos una funcion que se invoca a si misma, se invoca sola, la funcion pregunta constantemente
+// hay algo guardado en el localStorage y si es asi lo retorna al estado inicial y si no devuelve el local
+const initialState: UserWithId[] = (() => {
+  const persistedState = localStorage.getItem("__redux__state__");
+  if(persistedState){
+    return JSON.parse(persistedState).users;
+  }
+  return DEFAULT_STATE;
+})();
 
 export const usersSlice = createSlice({
     name: "users",
